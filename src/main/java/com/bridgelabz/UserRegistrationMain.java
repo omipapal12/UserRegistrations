24898dd99958a3879da2da00b3500914606a4415
omipapal12 <omipapal12@gmail.com>
2021-08-06 13:59:27 +0530

    Added: Valid Mobile Number

--- src/main/java/com/bridgelabz/UserRegistrationMain.java
+++ src/main/java/com/bridgelabz/UserRegistrationMain.java
@@ -9,5 +9,6 @@ public class UserRegistrationMain {
         userRegistration.validFirstName();
         userRegistration.validLastName();
         userRegistration.validEmail();
+        userRegistration.validMobileNumber();
     }
 }
