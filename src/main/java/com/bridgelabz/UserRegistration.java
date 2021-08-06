3e621dea0329b9bef4a303b15f44666ca05b188a
omipapal12 <omipapal12@gmail.com>
2021-08-06 13:30:13 +0530

    Added: Valid First Name

---
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -0,0 +1,15 @@
+package com.bridgelabz;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class UserRegistration {
+    /*
+     1) First Name start with Capital letter and it has minimum 3 charachter
+     */
+    public static void validFirstName() {
+        System.out.print("Enter First Name : ");
+        Scanner scanner = new Scanner(System.in);
+        String FirstName = scanner.next();
+        System.out.println(Pattern.matches("[A-Z][a-z]{2,}", FirstName));
+    }
+}
